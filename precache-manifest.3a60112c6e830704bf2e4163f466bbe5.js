6dcc1480909b457f47f9d8d160f1290011450743
techSavvySaahil <saahil.madaan@gmail.com>
2019-07-21 21:30:49 +0530

    Updates

---
+++ precache-manifest.3a60112c6e830704bf2e4163f466bbe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c090d72f54d03387cdd477389d6f3c5",
+    "url": "/would-you-game/index.html"
+  },
+  {
+    "revision": "b7974cd36de85eddf6e1",
+    "url": "/would-you-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ffeadff60f74114970cc",
+    "url": "/would-you-game/static/js/2.4ea3c6e7.chunk.js"
+  },
+  {
+    "revision": "b7974cd36de85eddf6e1",
+    "url": "/would-you-game/static/js/main.4be3f734.chunk.js"
+  },
+  {
+    "revision": "33960c79e996ddf16d6c",
+    "url": "/would-you-game/static/js/runtime~main.bb72fed3.js"
+  }
+]);
